88db916026af1883ae78435b2bac23a307ce52b2
Pankaj <92032149+pk2453@users.noreply.github.com>
2025-07-22 15:07:32 +0100

    fix: fix vulnerability (#1)
    * fix: fix vulnerability
    
    * fix: fix vulnerability
    
    * fix: fix vulnerability

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.2"
   kotlin("plugin.spring") version "2.1.21"
+}
+
+dependencyCheck {
+  suppressionFile = "/src/main/resources/dependency-suppression.xml"
 }
 
 configurations {
