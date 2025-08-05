dd6a48e88f2130dba4a69d60ee9dbfae51c49561
Pankaj Khare <pankaj.khare1@justice.gov.uk>
2025-08-05 11:12:45 +0100

    add event output to data class

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/entity/EventData.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/entity/EventData.kt
@@ -1,7 +1,11 @@
 package uk.gov.justice.digital.hmpps.esurveillanceapi.entity
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class EventData(
-  val personId: String?,
-  val eventName: String?,
-  val timestamp: String?,
+  @JsonProperty("person_id") val personId: String?,
+  @JsonProperty("event_name") val eventName: String?,
+  @JsonProperty("timestamp") val timestamp: String?,
 )
