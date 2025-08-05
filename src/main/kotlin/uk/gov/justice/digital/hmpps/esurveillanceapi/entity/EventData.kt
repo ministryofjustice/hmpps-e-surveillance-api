dbd05c8f08a6a73b5cd9229d343df5e5cd0465c5
Pankaj Khare <pankaj.khare1@justice.gov.uk>
2025-08-05 11:05:35 +0100

    add event output to data class

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/entity/EventData.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/entity/EventData.kt
@@ -3,5 +3,5 @@ package uk.gov.justice.digital.hmpps.esurveillanceapi.entity
 data class EventData(
   val personId: String?,
   val eventName: String?,
-  val timestamp: String?
+  val timestamp: String?,
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/service/EventsProcessorService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/service/EventsProcessorService.kt
@@ -42,7 +42,7 @@ class EventsProcessorService(private val s3ClientBuilderService: S3ClientBuilder
           EventData(
             personId = row["person_id"]?.removeSurrounding("'"),
             eventName = row["event_name"]?.removeSurrounding("'"),
-            timestamp = row["timestamp"]?.removeSurrounding("'")
+            timestamp = row["timestamp"]?.removeSurrounding("'"),
           )
         }
       LOG.info("Events received for $personId $eventData")
