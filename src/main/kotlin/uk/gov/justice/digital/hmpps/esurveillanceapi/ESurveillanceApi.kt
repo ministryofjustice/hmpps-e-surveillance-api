c03f131b0c4ed5b439eee06ce6cd609aa4493729
Denis Davidov <denislav.davivod@justice.gov.uk>
2025-08-08 12:28:22 +0100

    removing unused import

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/ESurveillanceApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/ESurveillanceApi.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.esurveillanceapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
