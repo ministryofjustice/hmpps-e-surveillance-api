a625c02ba41548b2c7809d77332f573e7722514d
Pankaj Khare <pankaj.khare1@justice.gov.uk>
2025-08-19 09:55:47 +0100

    disable auth in dev

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/config/SecurityConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/config/SecurityConfig.kt
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile
 import uk.gov.justice.hmpps.kotlin.auth.dsl.ResourceServerConfigurationCustomizer
 
 @Configuration
-@Profile("local")
+@Profile("dev", "local")
 class SecurityConfig {
   @Bean
   fun resourceServerCustomizer() = ResourceServerConfigurationCustomizer {
