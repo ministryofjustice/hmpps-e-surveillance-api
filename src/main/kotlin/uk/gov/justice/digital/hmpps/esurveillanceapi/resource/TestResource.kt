522b98d23f644b3ebd7f7c3f292340b92525c314
Pankaj Khare <pankaj.khare1@justice.gov.uk>
2025-08-20 12:14:04 +0100

    modify topic name

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/resource/TestResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/resource/TestResource.kt
@@ -1,9 +1,16 @@
 package uk.gov.justice.digital.hmpps.esurveillanceapi.resource
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.sns.SnsClient
+import software.amazon.awssdk.services.sns.model.SubscribeRequest
+import uk.gov.justice.digital.hmpps.esurveillanceapi.resource.IngestResource.Companion.LOG
+import java.net.URI
 
 @RestController
 @RequestMapping("/test")
@@ -11,4 +18,52 @@ class TestResource {
 
   @GetMapping("/hello")
   fun sayHello(@RequestParam(name = "name", defaultValue = "World") name: String): String = "Hello, $name!"
+
+  @Value("\${aws.sns.endpoint}")
+  private lateinit var snsEndpoint: String
+
+  @Value("\${aws.region}")
+  private lateinit var region: String
+
+  @Value("\${aws.topic-arn.file-upload}")
+  private lateinit var fileUploadTopicArn: String
+
+  @Value("\${aws.topic-arn.events}")
+  private lateinit var eventsTopicArn: String
+
+  @Value("\${end-point.ingest}")
+  private lateinit var ingestEndPoint: String
+
+  @Value("\${end-point.ingest-events}")
+  private lateinit var ingestEventsEndPoint: String
+
+  @GetMapping(value = ["/subscribe/{topic}"])
+  fun subscribe(@PathVariable topic: String) {
+    LOG.info("Received subscription request for: $topic")
+    val snsClient = buildSnsClient()
+    if (topic.contains("8981184ea3c74a9ee7121999e7f78b43")) {
+      val subscribeRequest = SubscribeRequest.builder()
+        .topicArn(fileUploadTopicArn)
+        .protocol("http")
+        .endpoint(ingestEndPoint)
+        .build()
+
+      val subscribeResponse = snsClient.subscribe(subscribeRequest)
+      LOG.info("Subscribed: ${subscribeResponse.subscriptionArn()}")
+    } else if (topic.contains("15cb40d70c799eb857f390669c0375be")) {
+      val subscribeRequest = SubscribeRequest.builder()
+        .topicArn(eventsTopicArn)
+        .protocol("http")
+        .endpoint(ingestEventsEndPoint)
+        .build()
+
+      val subscribeResponse = snsClient.subscribe(subscribeRequest)
+      LOG.info("Subscribed: ${subscribeResponse.subscriptionArn()}")
+    }
+  }
+
+  private fun buildSnsClient(): SnsClient = SnsClient.builder()
+    .endpointOverride(URI.create(snsEndpoint))
+    .region(Region.of(region))
+    .build()
 }
