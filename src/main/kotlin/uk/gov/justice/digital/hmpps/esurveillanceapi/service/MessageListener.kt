35895b47f1374b2b1d5c66cdab4f0edc432e5204
denislav-davidov-moj <denislav.davidov@justice.gov.uk>
2025-09-05 15:51:01 +0100

    adding logging sqs processes (#55)
    Co-authored-by: Denis Davidov <denislav.davivod@justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/service/MessageListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/service/MessageListener.kt
@@ -2,6 +2,8 @@ package uk.gov.justice.digital.hmpps.esurveillanceapi.service
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.awspring.cloud.sqs.annotation.SqsListener
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.RequestBody
 
@@ -11,15 +13,29 @@ class MessageListener(
   private val eventsProcessorService: EventsProcessorService,
 ) {
 
+  companion object {
+    private val LOG: Logger = LoggerFactory.getLogger(MessageListener::class.java)
+  }
+
   @SqsListener("fileuploadqueue", factory = "hmppsQueueContainerFactoryProxy")
   fun processFileUploadMessage(@RequestBody rawMessage: String) {
     val outerJson = jacksonObjectMapper().readTree(rawMessage)
-    fileProcessorService.processUploadedFile(outerJson)
+    try {
+      fileProcessorService.processUploadedFile(outerJson)
+    } catch (e: Exception) {
+      LOG.error("Failed to process message from fileuploadqueue: $outerJson", e)
+      throw e
+    }
   }
 
   @SqsListener("personidqueue", factory = "hmppsQueueContainerFactoryProxy")
   fun processPersonIfdMessage(@RequestBody rawMessage: String) {
     val outerJson = jacksonObjectMapper().readTree(rawMessage)
-    eventsProcessorService.processPersonId(outerJson)
+    try {
+      eventsProcessorService.processPersonId(outerJson)
+    } catch (e: Exception) {
+      LOG.error("Failed to process message from personidqueue: $outerJson", e)
+      throw e
+    }
   }
 }
