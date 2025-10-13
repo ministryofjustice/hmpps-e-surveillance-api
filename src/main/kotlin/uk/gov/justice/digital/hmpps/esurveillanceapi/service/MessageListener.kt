656598aebde714a87d40ce7c2d1ba9aac21d8c0f
Pankaj Khare <pankaj.khare1@justice.gov.uk>
2025-10-13 09:46:08 +0100

    ESUR-113 Replace custom message generator with Gov.UK Notify generated message

--- src/main/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/service/MessageListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/service/MessageListener.kt
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.awspring.cloud.sqs.annotation.SqsListener
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.RequestBody
 
+@ConditionalOnProperty(name = ["hmpps.sqs.enabled"], havingValue = "true", matchIfMissing = true)
 @Service
 class MessageListener(
   private val fileProcessorService: FileProcessorService,
