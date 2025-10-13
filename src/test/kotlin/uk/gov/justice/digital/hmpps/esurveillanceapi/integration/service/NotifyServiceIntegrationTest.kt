fc8ede45f6da1f943dd35602f3188732153944e6
Pankaj Khare <pankaj.khare1@justice.gov.uk>
2025-10-13 09:36:54 +0100

    ESUR-113 Replace custom message generator with Gov.UK Notify generated message

--- src/test/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/integration/service/NotifyServiceIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/esurveillanceapi/integration/service/NotifyServiceIntegrationTest.kt
@@ -10,9 +10,10 @@ import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.core.env.Environment
 import org.springframework.test.context.ActiveProfiles
 import uk.gov.justice.digital.hmpps.esurveillanceapi.service.NotifyService
+import uk.gov.justice.hmpps.sqs.HmppsQueue
+import uk.gov.justice.hmpps.sqs.HmppsQueueService
 import uk.gov.service.notify.NotificationClient
 import uk.gov.service.notify.SendEmailResponse
 import uk.gov.service.notify.SendSmsResponse
@@ -22,19 +23,20 @@ import java.util.UUID
 @ActiveProfiles("test")
 class NotifyServiceIntegrationTest {
 
+  @MockBean
+  lateinit var hmppsQueueService: HmppsQueueService
+
+  @MockBean(name = "fileuploadqueue")
+  lateinit var fileUploadQueue: HmppsQueue
+
+  @MockBean(name = "fileuploadqueue-dlq")
+  lateinit var fileUploadDlq: HmppsQueue
+
   @MockBean(name = "primaryNotifyClient")
   lateinit var primaryNotifyClient: NotificationClient
 
   @Autowired
   lateinit var notifyService: NotifyService
-
-  @Autowired
-  lateinit var environment: Environment
-
-  @Test
-  fun checkProperty() {
-    println("hmpps.sqs.enabled = " + environment.getProperty("hmpps.sqs.enabled"))
-  }
 
   @Test
   fun `should use primary notify client to send email`() {
